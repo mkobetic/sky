bfcbd51ecd6951395d0aaf8f524232fdd26713bf
Martin Kobetic <mkobetic@gmail.com>
2014-01-29 16:49:43 +0000

    add shard_test

--- db/shard_test.go
+++ db/shard_test.go
@@ -10,17 +10,17 @@ import (
 
 func TestShardStat(t *testing.T) {
 	withShard(func(s *shard) {
-		s.InsertEvent("tbl0", "obj0", testevent("2000-01-02T00:00:00Z", 1, "xxx"))
-		s.InsertEvent("tbl0", "obj0", testevent("2000-01-01T00:00:00Z", 1, "yyy"))
-		s.InsertEvent("tbl0", "obj1", testevent("2000-01-03T00:00:00Z", 1, "zzz"))
-		s.InsertEvent("tbl1", "obj0", testevent("2000-01-05T00:00:00Z", 1, "zzz"))
-		s.InsertEvent("tbl1", "obj0", testevent("2000-01-06T00:00:00Z", 1, "zzz"))
+		s.InsertEvent("tbl0", "obj0", testevent("2000-01-02T00:00:00Z", 1, "xxx"), false)
+		s.InsertEvent("tbl0", "obj0", testevent("2000-01-01T00:00:00Z", 1, "yyy"), false)
+		s.InsertEvent("tbl0", "obj1", testevent("2000-01-03T00:00:00Z", 1, "zzz"), false)
+		s.InsertEvent("tbl1", "obj0", testevent("2000-01-05T00:00:00Z", 1, "zzz"), false)
+		s.InsertEvent("tbl1", "obj0", testevent("2000-01-06T00:00:00Z", 1, "zzz"), false)
 		stat, err := s.Stat()
 		assert.Nil(t, err, "")
 		assert.Equal(t, stat.Entries, uint64(2))
 		assert.Equal(t, stat.Size, uint64(0x20000000000))
 		assert.Equal(t, stat.Depth, uint(1))
-		assert.Equal(t, stat.Transactions.Last, uint64(5))
+		assert.Equal(t, stat.Transactions.Last, uint64(7))
 		assert.Equal(t, stat.Readers.Max, uint(126))
 		assert.Equal(t, stat.Readers.Current, uint(0))
 		assert.Equal(t, stat.Pages.Last, uint64(10))
@@ -36,7 +36,7 @@ func withShard(f func(*shard)) {
 	defer os.RemoveAll(path)
 
 	s := newShard(path)
-	if err := s.Open(4096, 126, options(false)); err != nil {
+	if err := s.Open(4096, 126, 0); err != nil {
 		panic(err.Error())
 	}
 	defer s.Close()
